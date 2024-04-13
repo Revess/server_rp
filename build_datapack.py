79dfb58f123a8c148b65538667a4c4859b215dc6
bas <bjmaat@gmail.com>
2024-04-13 17:32:32 +0200

    Working chances

--- build_datapack.py
+++ build_datapack.py
@@ -29,13 +29,15 @@ def create_item(item, weight, min, max) -> dict:
     }
 
 def find_correct_loot_table(path, loot_table):
+    found_ = []
     for subfolder in glob.glob(f'{path}*'):
         if os.path.isdir(subfolder):
             path = find_correct_loot_table(f'{subfolder}/', loot_table)
             if path is not None:
-                return path
+                found_.extend(path)
         elif loot_table in subfolder.split('/')[-1] and os.path.isfile(subfolder):
-            return subfolder
+            found_.append(subfolder)
+    return found_
 
 def create_datapack():
     # Setup the folder structure
@@ -55,21 +57,22 @@ def create_datapack():
             if os.path.exists(loot_path):
                 for loot_table, tables in rules[namespace].items():
                     # Find the files and copy them over
-                    found_ = find_correct_loot_table(loot_path, loot_table)
-                    if found_:
-                        path = './data/'
-                        for folder_ in found_.split('/')[2:]: 
-                            if 'json' not in folder_:
-                                path = f'{path}{folder_}/'
-                                os.makedirs(path, exist_ok=True)
-                        shutil.copy(found_, path)
-                        found_ = found_.replace('originals', 'data')
-                        with open(found_, 'r') as file_:
-                            old_loot_table = json.load(file_)
-                        for table in tables:
-                            old_loot_table["pools"][0]["entries"].append(create_item(table[0], table[1], table[2], table[3]))
-                        with open(found_, 'w') as file_:
-                            json.dump(old_loot_table, file_)
+                    founds_ = find_correct_loot_table(loot_path, loot_table)
+                    for found_ in founds_:
+                        if found_:
+                            path = './data/'
+                            for folder_ in found_.split('/')[4:]: 
+                                if 'json' not in folder_:
+                                    path = f'{path}{folder_}/'
+                                    os.makedirs(path, exist_ok=True)
+                            shutil.copy(found_, path)
+                            found_ = found_.replace(f'./originals/{namespace}/data/', './data/')
+                            with open(found_, 'r') as file_:
+                                old_loot_table = json.load(file_)
+                            for table in tables:
+                                old_loot_table["pools"][0]["entries"].append(create_item(table[0], table[1], table[2], table[3]))
+                            with open(found_, 'w') as file_:
+                                json.dump(old_loot_table, file_)
 
 if __name__ == "__main__":
     fire.Fire(create_datapack)
